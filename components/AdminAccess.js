899efc2d721581bd100d1bc8c59925392a24c97d
Jason <jasonhe@udel.edu>
2022-04-21 12:44:47 -0400

    Added spreadsheets to AdminAccess

--- components/AdminAccess.js
+++ components/AdminAccess.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
 
+import exportExcel from './Spreadsheets'
+
 
 export default function AdminAccess() {
+
+    const info = "Jason He$4/12/2022$B73_Plant01$06/01/2020$A$0.751 +/- 0.032$No Notes"
+
     return (
         <View style={styles.container}>
-            <Text>This is Text</Text>
+            <Text>View your session data</Text>
+
+            <Button
+                title="Export Excel"
+                onPress={() => exportExcel(info)} />
         </View>
     )
 }
