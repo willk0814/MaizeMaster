3147a27bf0552a3429ae7a87d432fc9e8bd0d57e
Jason <jasonhe@udel.edu>
2022-04-26 14:04:53 -0400

    Multiple files now downloadable

--- components/AdminAccess.js
+++ components/AdminAccess.js
@@ -1,20 +1,71 @@
-import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
+import React, { useState } from 'react'
+import { FlatList, StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 
 import exportExcel from './Spreadsheets'
 
 
 export default function AdminAccess() {
 
-    const info = "Jason He$4/12/2022$B73_Plant01$06/01/2020$A$0.751 +/- 0.032$No Notes"
+    let DATA = [
+        {
+            id: "Tim A$4/12/2022$A73_Plant01$06/01/2020$A$0.751 +/- 0.032$Example A",
+            title: "TA A73_Plant01",
+        },
+        {
+            id: "Jane B$4/10/2022$B99_Plant02$08/01/2020$A$0.631 +/- 0.025$Example B",
+            title: "JB B99_Plant02",
+        },
+        {
+            id: "Bob C$4/14/2022$C12_Plant03$01/01/2020$A$0.810 +/- 0.038$Example C",
+            title: "BC C12_Plant03",
+        },
+        {
+            id: "Tim A$4/12/2022$A73_Plant04$06/01/2020$A$0.751 +/- 0.032$Example D",
+            title: "TA A73_Plant04",
+        },
+        {
+            id: "Jane B$4/10/2022$B99_Plant05$08/01/2020$A$0.631 +/- 0.025$Example E",
+            title: "JB B99_Plant05",
+        },
+        {
+            id: "Bob C$4/14/2022$C12_Plant06$01/01/2020$A$0.810 +/- 0.038$Example F",
+            title: "BC C12_Plant06",
+        },
+    ];
+    
+    const [selectedId, setSelectedId] = useState(null);
+
+    const Item = ({ item, onPress, backgroundColor, textColor }) => (
+        <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+            <Text style={[styles.title, textColor]}>{item.title}</Text>
+        </TouchableOpacity>
+    );
+    
+    const renderItem = ({ item }) => {
+        const backgroundColor = item.id === selectedId ? "#05611d" : "#99ffb4";
+        const color = item.id === selectedId ? 'white' : 'black';
+    
+        return (
+            <Item
+                item={item}
+                onPress={() => [setSelectedId(item.id), exportExcel(item.id)]}
+                backgroundColor={{ backgroundColor }}
+                textColor={{ color }}
+            />
+        );
+    };
 
     return (
         <View style={styles.container}>
-            <Text>View your session data</Text>
-
-            <Button
-                title="Export Excel"
-                onPress={() => exportExcel(info)} />
+            <Text style ={styles.title}>View your session data</Text>
+            <View style={styles.list}>
+                <FlatList
+                    data={DATA}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id}
+                    extraData={selectedId}
+                />
+            </View>
         </View>
     )
 }
@@ -23,6 +74,22 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#fff'
-    }
+        backgroundColor: '#fff',
+    },
+    list: {
+        padding: 10,
+        margin: 10,
+        borderWidth: 2,
+        borderRadius: 20
+    },
+    item: {
+        width: 300,
+        backgroundColor: '#a2ffcb',
+        padding: 5,
+        marginVertical: 4,
+        fontSize: 12
+    },
+    title: {
+        fontSize: 32,
+    },
 })
